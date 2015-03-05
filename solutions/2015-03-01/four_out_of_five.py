704392505fb0b6336b84bd71bbf74f752e45601c
leiran <leiranbiton@gmail.com>
2015-03-04 20:43:17 -0500

    Added stub method

--- solutions/2015-03-01/four_out_of_five.py
+++ solutions/2015-03-01/four_out_of_five.py
@@ -5,3 +5,20 @@ and a five-letter word to get a pair of synonyms.
 For example, given 'time' and 'night,' you would say 'item' and 'thing.'
 """
 
+# imports
+from nltk.corpus import wordnet
+
+class main:
+    def __init__(self
+                ,DEBUG=False)
+                :
+        self.DEBUG = DEBUG
+    
+    def synonyms(self
+                ,word
+                ,DEBUG=False)
+                :
+        return wn.synsets(word)
+    
+if __name__ == "__main__":
+    m = main(DEBUG=True)
