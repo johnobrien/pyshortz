ee80338c62cf83ec668fc2a3658d659693b01359
John OBrien <john.obrien@gmail.com>
2015-03-04 20:27:08 -0500

    Added the next puzzler.

---
+++ solutions/2015-03-01/four_out_of_five.py
@@ -0,0 +1,7 @@
+"""
+Created on Mar 3, 2015
+Rearrange the letters in a four-letter word 
+and a five-letter word to get a pair of synonyms. 
+For example, given 'time' and 'night,' you would say 'item' and 'thing.'
+"""
+
