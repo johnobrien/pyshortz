c2fe676beb86b307f31541d2bb2eacdb6a5c074c
John.OBrien <John.OBrien@MA10LP509047.na.wkglobal.com>
2015-03-23 09:27:28 -0400

    Got the basic frameworking working. Though the critical piece in the middle isn't working so well...

--- solutions/2015-03-01/four_out_of_five.py
+++ solutions/2015-03-01/four_out_of_five.py
@@ -5,23 +5,33 @@ and a five-letter word to get a pair of synonyms.
 For example, given 'time' and 'night,' you would say 'item' and 'thing.'
 """
 # imports
+import nltk
 from nltk.corpus import wordnet
 import requests
-
+import shelve
 
 
 class main:
     def __init__(self
                 ,DEBUG=False):
-        # We should write something so that if
-        # Wordnet has not already been downloaded,
-        # wordnet is downloaded on first use...
-        # http://www.velvetcache.org/2010/03/01/looking-up-words-in-a-dictionary-using-python for reference
-        
+        # For reference http://www.velvetcache.org/2010/03/01/looking-up-words-in-a-dictionary-using-python for reference
+        nltk.download('wordnet')
         self.DEBUG = DEBUG
-        r = requests.get('http://www-personal.umich.edu/~jlawler/wordlist', timeout=10)
-        self.words = r.text.split("\n")
-        self.hashed_words = [self.hashify(word) for word in self.words] 
+        
+        d = shelve.open("four_out_of_five.db")
+        print("Loading list of all english words...")
+        if d.has_key("words") and d.has_key("hashed_words"):
+            self.words = d["words"]
+            self.hashed_words = d["hashed_words"]
+        else:
+            print("Stored list not found.\n")
+            print("Downloading a list of all words in the english language...\n")
+            r = requests.get('http://www-personal.umich.edu/~jlawler/wordlist', timeout=10)
+            self.words = r.text.split("\r\n")
+            self.hashed_words = [self.hashify(word) for word in self.words] 
+            d["words"] = self.words
+            d["hashed_words"] = self.hashed_words
+            d.close()
 
     def synonyms(self
                 ,word
@@ -38,23 +48,42 @@ class main:
         
         # We loop through each word in the english language, looking for ones that
         # have synonyms
+        winners = []
+        
         for word in self.words:
+            print("Trying {0}".format(word))
             synsets =  wordnet.synsets(word)
-            if len(synsets) > 1:
-                # This words has synonyms!
-                for syn1 in synset:
-                    if len(syn1) == 4:
-                        # The first synonym is four letters long!
-                        for syn2 in synset:
-                            if len(syn2) == 5:
-                                # The second synonym is five letters long!
-                                hash_syn1 = self.hashify(syn1)
-                                hash_syn2 = self.hashify(syn2)
-                                if hash_syn1 in self.hashed_words and hash_syn2 in self.hashed_words:
-                                    if syn1 != hashed_syn1 and syn2 != hashed_syn2:
-                                        print("We have a winner!")
-                                        print("Synonym 1:{0}\n").format(syn1)
-                                        print("Synonym 2:{0}\n").format(syn2)
+            print("Number of synsets for {0} is {1}.".format(word, len(synsets)))
+            for synset in synsets:
+                print("Seeing how many synonyms for first usage of {0}".format(synset.name()))
+                if len(synset.lemma_names()) > 1:
+                    print("{0} has synonyms! \n".format(word))
+                    syns = synset.lemma_names()
+                    for syn1 in syns:
+                        print("Checking first synonym {0}...\n".format(syn1))
+                        if len(syn1) == 4:
+                            # The first synonym is four letters long!
+                            for syn2 in syns:
+                                if len(syn2) == 5:
+                                    # The second synonym is five letters long!
+                                    # TODO: Leiran, this section doesn't work
+                                    # I think we need someway to check that the hashed version
+                                    # of the word has a correlate that is not the same as the original word
+                                    # Maybe of dictionary, where the key is the hashed word and the value
+                                    # is the original word?
+                                    hashed_syn1 = self.hashify(syn1)
+                                    hashed_syn2 = self.hashify(syn2)
+                                    if hashed_syn1 in self.hashed_words and hashed_syn2 in self.hashed_words:
+                                        if syn1 != hashed_syn1 and syn2 != hashed_syn2:
+                                            winner = [syn1, syn2]
+                                            winners.append(winner) 
+                                            print("We have a winner!")
+                                            print("Synonym 1:{0}\n").format(syn1)
+                                            print("Synonym 2:{0}\n").format(syn2)
+
+        for winner in winners:
+            print("Winner is {0} and {1}".format(winner[0], winner[1]))
+        print("Done.")
         return()
     
 if __name__ == "__main__":
