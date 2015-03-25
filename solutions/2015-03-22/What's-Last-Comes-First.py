086aad95293a9030400653d047679cffeb82df01
john.obrien <john.obrien@MA10LP509047.na.wkglobal.com>
2015-03-25 14:59:12 -0400

    Adding in this weeks puzzle!

---
+++ solutions/2015-03-22/What's-Last-Comes-First.py
@@ -0,0 +1,12 @@
+'''
+Created on Mar 25, 2015
+
+Take the word "die." Think of two synonyms 
+for this word that are themselves exact opposites 
+of each other. 
+
+What two words are these? A hint: they have the same number of letters.
+
+@author: john.obrien
+'''
+
