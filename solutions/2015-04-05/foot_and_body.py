4a2b2a0f8011bd7d4f708e1c96ffdfeffb87fb82
John.OBrien <John.OBrien@MA10LP509047.na.wkglobal.com>
2015-04-06 10:29:06 -0400

    Laid out a rough sketch of how to get a local dictionary of words.

---
+++ solutions/2015-04-05/foot_and_body.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3.4.2
+'''
+Created on Apr 6, 2015
+
+Name something that might be worn on the foot. 
+
+Change one letter in it without changing the 
+order of the other letters. The result 
+will name something one might wear on the 
+upper part of the body. What is it? 
+
+Here's a hint: The thing on the upper part 
+of the body is a two-word phrase.
+
+@author: john.obrien, leiran.biton
+'''
+
+'''
+I'm sick of trying to come up w/ lists 
+of things that are worn on the upper body
+or whatever.
+
+New plan: download a dictionary of all words,
+and search their definitions to get words that
+contain arbitrary words in their definition.
+
+Dictionary of words to download:
+https://github.com/adambom/dictionary
+
+How to search for words within the values of dictionaries:
+http://stackoverflow.com/questions/17340922/how-to-search-if-dictionary-value-contains-certain-string-with-python
+
+Rough sketch: download the JSON, 
+load it into a shelf, and search it.
+Probably add it to the "setup" page.
+
+'''
