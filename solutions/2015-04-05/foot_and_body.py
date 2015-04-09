9665f87609a52c87c50b14c7e5839b6dd99db1d2
leiran <leiranbiton@gmail.com>
2015-04-09 09:53:23 -0400

    Thursday morning revisions
    Improved implementation of search function. Results list is becoming
    more manageable, targeted, and comprehensive.

--- solutions/2015-04-05/foot_and_body.py
+++ solutions/2015-04-05/foot_and_body.py
@@ -31,14 +31,21 @@ were found in the definitions
 '''
 
 
-def search(kws):
+def search(kws, stype=all, word_type="noun"):
+    """
+kws   - list of keywords to be searched for
+stype - either all or any (default = all)
+word_type - part of speech (default = 'noun')
+"""
+    if stype not in (all, any): 
+        return TypeError("Invalid stype argument supplied. stype must be 'any' or 'all' functions.")
     matches = []
     # check kws against all defs in wordnets list of words
     for word in words.words():
         synset = wordnet.synsets(word)
         for syn in synset:
-            if any([kw.lower() in syn.definition().lower() for kw in kws]):
-                sys.stdout.write(".")
+            if stype([kw.lower() in syn.definition().lower() for kw in kws]) and \
+               syn.lexname().startswith(word_type):
                 matches.append(word.lower())
     return matches
 
@@ -58,15 +65,33 @@ if __name__ == "__main__":
     upperwear  = ["coot", "tshirt"]
     candidates = build_candidates(footwear, upperwear)
     for word, new_word in candidates:
-        print("    Footwear: {0}-> Upperwear:{1}".format(word, new_word))
+        print("    Footwear: {0}-> Upperwear: {1}".format(word, new_word))
+    
     print("Real run...")
-    footwear   = search(["foot", "feet"])
+    footwear   = search(["foot", "worn"], stype=all) 
+    footwear.extend(search(["feet", "worn"], stype=all))
+    footwear.extend(search(["foot", "wear"], stype=all))
+    footwear.extend(search(["feet", "wear"], stype=all))
+    footwear = list(set(footwear)) # remove duplicates
     print("Footwear: {0}".format(footwear))
-    upperwear  = search(["upper", "arm", "torso", "chest"])
+    
+    upperwear      = search(["upper", "wear"], stype=all)
+    upperwear.extend(search(["arm", "worn"], stype=all))
+    upperwear.extend(search(["torso", "worn"], stype=all))
+    upperwear.extend(search(["shoulder", "worn"], stype=all))
+    upperwear.extend(search(["chest", "worn"], stype=all))
+    upperwear.extend(search(["back", "worn"], stype=all))
+    upperwear.extend(search(["upper", "wear"], stype=all))
+    upperwear.extend(search(["arm", "wear"], stype=all))
+    upperwear.extend(search(["torso", "wear"], stype=all))
+    upperwear.extend(search(["shoulder", "wear"], stype=all))
+    upperwear.extend(search(["chest", "wear"], stype=all))
+    upperwear.extend(search(["back", "wear"], stype=all))
+    upperwear = list(set(upperwear)) # remove duplicates
     print("Upperwear: {0}".format(upperwear))
     candidates = build_candidates(footwear, upperwear)
     if len(candidates) is 0:
         print("No candidates found.")
     else:
         for word, new_word in candidates:
-            print("    Footwear: {0}-> Upperwear:{1}".format(word, new_word))
+            print("    Footwear: {0}-> Upperwear: {1}".format(word, new_word))
