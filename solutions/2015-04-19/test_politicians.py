ea180ca61a28e7143841ce5ac9e84a1fb1076a01
John.OBrien <John.OBrien@MA10LP509047.na.wkglobal.com>
2015-04-20 12:43:22 -0400

    Tweaked and fixed a bug. Added more tests. No solution yet.

--- solutions/2015-04-19/test_politicians.py
+++ solutions/2015-04-19/test_politicians.py
@@ -4,12 +4,21 @@ from politicians import solution
 class TestSolution(unittest.TestCase):
     def test_two_names(self):
         names = ("Hlbmuh", "Ecuoc")
-        switched = ("humble", "couch")
         s = solution(*names)
-        self.assertEqual(s.candidates, [names + switched], "Hlbmuh and Ecuoc did not generate humble and couch.")
+        self.assertEqual(s.candidates, [("Hlbmuh", "Ecuoc", "humble", "couch")], "Hlbmuh and Ecuoc did not generate humble and couch.")
 
     def test_three_names(self):
-        pass
+        names = ("Hlbmuh", "Ecuoc", "Parsley")
+        s = solution(*names)
+        self.assertEqual(s.candidates, [("Hlbmuh", "Ecuoc", "humble", "couch")], "Hlbmuh and Ecuoc did not generate humble and couch.")
+
+    def test_four_names(self):
+        names = ("Ben", "Ted", "Tim", "Don")
+        switched = [('Ben', 'Ted', 'net', 'deb'),
+                    ('Ben', 'Tim', 'net', 'mib'),
+                    ('Tim', 'Don', 'mid', 'not')]
+        s = solution(*names)
+        self.assertEqual(s.candidates, switched, "Hlbmuh and Ecuoc did not generate humble and couch.")
 
 if __name__ == "__main__":
     unittest.main()
