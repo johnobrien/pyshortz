c94af9f11f09a9aab07509a629bcb34953b8198e
johno_000 <johno_000@john-p34wv3cf2.home>
2015-04-26 20:58:20 -0500

    So far so good.

---
+++ solutions/2015-04-26/test_three_actors.py
@@ -0,0 +1,59 @@
+#!/usr/local/bin/env python
+
+'''
+Created on Apr 26, 2015
+
+@authors: Leiran Biton, John O'Brien
+'''
+"""Problem:
+Name a famous actor whose first and last names both are 
+seven letters long. Change the first three letters of 
+the actor's last name to three new letters and you'll 
+name another famous actor. They share the same first 
+name. Add the three letters you changed in the first 
+actor's last name plus the three letters you changed 
+to get the second actor's name, and you'll spell the 
+last name of a third famous actor. 
+
+Who are these three Hollywood stars?
+"""
+
+import unittest
+from three_actors import Solution
+
+class TestSolution(unittest.TestCase):
+    
+    def test_first_actor(self):
+        actor = "Earnest Fleming"
+        s = Solution(None)
+        self.assertTrue(s.first_actor("Earnest Fleming"))
+
+    def test_second_actor(self):
+        actor1 = "Earnest Fleming"
+        actor2 = "Earnest Troming"
+        s = Solution(None)
+        self.assertTrue(s.second_actor(actor1, actor2))
+
+    def test_third_actor(self):
+        actor1 = "Earnest Fleming"
+        actor2 = "Earnest Troming"
+        actor3 = "Killing Fletr"
+        s = Solution(None)
+        self.assertTrue(s.second_actor(actor1, actor2, actor3))
+
+
+
+    def test_three_actors(self):
+        actors = {"Earnest Fleming",
+                  "Earnest Troming",
+                  "Killing Fletr"}
+        possible_answers = {("Earnest Fleming",
+                                 "Earnest Troming",
+                                 "Killing Fletr")}
+        s = Solution(actors)
+        self.assertEqual(possible_answers, s.possible_answers)
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
---
+++ solutions/2015-04-26/three_actors.py
@@ -0,0 +1,129 @@
+#!/usr/local/bin/env python
+'''
+Created on Apr 26, 2015
+
+@authors: Leiran Biton, John O'Brien
+'''
+"""solution to the 4-19-2015 NPR puzzle for pyshortz blog."""
+
+problem = """Problem:
+Name a famous actor whose first and last names both are 
+seven letters long. Change the first three letters of 
+the actor's last name to three new letters and you'll 
+name another famous actor. They share the same first 
+name. Add the three letters you changed in the first 
+actor's last name plus the three letters you changed 
+to get the second actor's name, and you'll spell the 
+last name of a third famous actor. 
+
+Who are these three Hollywood stars?
+"""
+
+
+class Solution(object):
+    '''
+    Solution object
+    
+    '''
+    def first_actor(self, actor):
+        '''
+        Checks to see whether a string meets
+        the requirements to be the first actor
+        
+        Args:
+            actor (str): the name of a famous actor where the first and last name
+                        are both seven letters long
+        Returns:
+            True if the requirements are met, False if they are not
+
+
+        '''
+        first, last = actor.split()
+        return len(first) == 7 and len(last) == 7
+        
+
+    def second_actor(self, actor1, actor2):
+        '''
+        Checks to see whether given the first actor
+        a string meets the requirements to be the 
+        second actor
+        
+        
+        Args:
+            actor1 (str): the name of a famous actor where the first and last name
+                        are both seven letters long
+            actor2 (str): the name of the famous actor who shares the same first name
+                        as actor 1, and for whom the rest of his last name, excluding the
+                        first three letters, matches actor1
+        Returns:
+            True if the requirements are met, False if they are not
+            
+        '''
+        first1, last1 = actor1.split()
+        first2, last2 = actor2.split()
+        
+        return first1 == first2 and last1[3:] == last2[3:]
+
+    def third_actor(self, actor1, actor2, actor3):
+        '''
+        Checks to see whether given the first and second actors
+        a string meets the requirements to be the third actor
+        
+        
+        Args:
+            actor1 (str): the name of a famous actor where the first and last name
+                        are both seven letters long
+            actor2 (str): the name of the famous actor who shares the same first name
+                        as actor 1, and for whom the rest of his last name, excluding the
+                        first three letters, matches actor1
+            actor (str): the name of a famous actor whose last name is comprised of the first
+                        three letters of the actor1's last name combined with the first three letters
+                        of the actor2's last name
+        Returns:
+            True if the requirements are met, False if they are not
+            
+        '''
+        first1, last1 = actor1.split()
+        first2, last2 = actor2.split()
+        first3, last3 = actor3.split()
+ 
+        return last3 == last1[:3] + last2[:3]
+
+    def process_actors(self, actors):
+        '''
+        Searches a set of actors to find three that meet the requirements
+        
+        Args:
+            actors (set): a set of actors names, each of which is two words long
+        Returns:
+            possible_answers (set): a set of tuples, each of which is comprised
+                                    of three strings of the names of three actors who
+                                    meet the requirements
+            
+        '''
+        possible_answer  = set()
+        for actor1 in actors:
+            for actor2 in actors:
+                for actor3 in actors:
+                    if actor_one(actor1) and actor_two(actor2) and actor_three(actor3):
+                        possible_answer.update((actor2, actor2, actor3))
+
+        return possible_answers
+
+
+    def __init__(self, actors):
+        '''
+        Args:
+            actor (set): A list of strings, each of which is the first and last name of an actor
+                    
+        Example self.possible_answers value:
+            possible_answers = {("Earnest Fleming",
+                                 "Earnest Troming",
+                                 "Killing Fletr")}
+            
+        '''
+        self.possible_answers = self.process_actors(actors)
+
+if __name__ == "__main__":
+    print("\n"+problem)
+
