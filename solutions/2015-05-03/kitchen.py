7db6f049b2b863270fb5dee10a9f60bfce9a694b
leiran <leiranbiton@gmail.com>
2015-05-04 16:05:48 -0400

    Refactored to include check for "leftover word"
    Result is now less clear, but probably more comprehensive.

--- solutions/2015-05-03/kitchen.py
+++ solutions/2015-05-03/kitchen.py
@@ -19,6 +19,7 @@ in one word, that you might prepare in a kitchen.
 
 What is it?
 """
+    allwords = words.words()
     
     # methods
     def __init__(self
@@ -30,6 +31,25 @@ What is it?
         if self.verbose:
             print(self.problem)
         
+    def word_check(self
+                  ,word
+                  ,larger_word
+                  ):
+        """checks whether the leftover portion of a word after a subword is removed is a word.
+        
+        args:
+        
+        word, larger_word"""
+        word_index = larger_word.find(word)
+        word_len   = len(word)
+        leftover_word = larger_word[:word_index] + larger_word[word_index + word_len:]
+        if leftover_word in self.allwords and len(leftover_word) > 2:
+            syns = self.wordnet.synsets(leftover_word)
+            for syn in syns: 
+                if syn.name().split(".")[0] == leftover_word:
+                    return leftover_word
+        return None
+    
     def word_switch(self
                    ,phrase
                    ):
@@ -49,7 +69,7 @@ to the parent object."""
         self.kitchen_items = []
         if self.verbose:
             print("populating kitchen_items list using nltk...")
-        for word in self.words.words():
+        for word in self.allwords:
             synset = self.wordnet.synsets(word)
             for syn in synset:
                 if any([kw.lower() in syn.definition().lower() for kw in keywords]):
@@ -67,7 +87,7 @@ to the parent object."""
         self.food = []
         if self.verbose:
             print("populating food list using nltk...")
-        for word in self.words.words():
+        for word in self.allwords:
             synset = self.wordnet.synsets(word)
             for syn in synset:
                 if any([kw.lower() in syn.definition().lower() for kw in keywords]):
@@ -83,8 +103,11 @@ to the parent object."""
         self.candidates = set()
         for k_item in self.kitchen_items:
             for food in self.food:
-                if k_item in food and k_item != food:
-                    self.candidates.add((k_item, food))
+                if (food.startswith(k_item) or food.endswith(k_item)) \
+                                                    and k_item != food:
+                    other_word = self.word_check(k_item, food)
+                    if other_word:
+                        self.candidates.add((k_item, other_word, food))
         if self.verbose: print("found %i possible partial solutions" % len(self.candidates))
         
 if __name__ == "__main__":
@@ -98,41 +121,131 @@ if __name__ == "__main__":
             ,"flatware"
             ,"silverware"
             ,"chef"
+            ,"appliance"
+            ,"implement"
+            ,"dish"
+            ,"dishes"
+            ,"fixture"
+            ,"cupboard"
+            ,"food"
             ]
     f_kws = ["food"
             ,"meat"
             ,"cheese"
             ,"bread"
             ,"vegetable"
+            ,"fruit"
             ,"dish"
             ,"recipe"
+            ,"meal"
+            ,"breakfast"
+            ,"lunch"
+            ,"sandwich"
+            ,"dinner"
+            ,"loaf"
+            ,"croquette"
+            ,"bean"
+            ,"beans"
+            ,"appetizer"
+            ,"dessert"
             ]
     s = Solution(DEBUG=True, verbose=True)
     s.populate_kitchen_items(k_kws)
     s.populate_food(f_kws)
     s.build_candidates()
-    for k_item, food in s.candidates:
-        print(k_item, "->", food)
+    for k_item, other_word, food in s.candidates:
+        print("{0} + {1} -> {2}".format(k_item, other_word, food))
     
-# found 18 possible partial solutions
-# ware -> chinaware
-# parer -> sparerib
-# cutler -> cutlery
-# oven -> ovenware
-# pan -> dishpan
-# grid -> griddle
-# beefwood -> scrub_beefwood
-# pan -> trepang
-# pan -> panfish
-# pan -> pompano
-# pan -> kalumpang
-# pan -> frying_pan
-# set -> russet
-# pan -> pantry
-# pan -> lesser_panda
-# ware -> ovenware
-# ware -> tableware
-# server -> preserver
+# found 18 possible partial solutions (pan fish)
+
+# found 105 possible partial solutions <-- removed 1-2 letter words
+# found 86 possible partial solutions
+# cook + book -> cookbook
+# bean + feast -> beanfeast
+# bread + stuff -> breadstuff
+# spice + all -> allspice
+# fish + wolf -> wolffish
+# pick + tooth -> toothpick
+# pie + pot -> potpie
+# meal + time -> mealtime
+# bread + basket -> breadbasket
+# fish + dollar -> dollarfish
+# ling + dump -> dumpling
+# meat + horse -> horsemeat
+# meat + less -> meatless
+# fast + break -> breakfast
+# ware + oven -> ovenware
+# board + cheese -> cheeseboard
+# board + bread -> breadboard
+# oven + ware -> ovenware
+# washer + dish -> dishwasher
+# sweet + sop -> sweetsop
+# dish + pan -> dishpan
+# pan + dish -> dishpan
+# sago + sap -> sapsago
+# fish + pork -> porkfish
+# oat + meal -> oatmeal
+# frog + bit -> frogbit
+# fish + cat -> catfish
+# black + berry -> blackberry
+# stick + chop -> chopstick
+# mess + mate -> messmate
+# bread + fruit -> breadfruit
+# grocer + green -> greengrocer
+# bean + stalk -> beanstalk
+# cheese + cake -> cheesecake
+# sop + sweet -> sweetsop
+# spoon + dessert -> dessertspoon
+# mars + ala -> marsala
+# ware + table -> tableware
+# egg + cup -> eggcup
+# milk + shake -> milkshake
+# mince + meat -> mincemeat
+# fish + sword -> swordfish
+# dishonor + able -> dishonorable
+# dish + washer -> dishwasher
+# ware + dinner -> dinnerware
+# ware + china -> chinaware
+# dish + rag -> dishrag
+# sop + sour -> soursop
+# pan + try -> pantry
+# fish + shell -> shellfish
+# serve + con -> conserve
+# egg + plant -> eggplant
+# eat + age -> eatage
+# pan + fish -> panfish
+# fish + weak -> weakfish
+# fish + dolphin -> dolphinfish
+# fish + spade -> spadefish
+# spoon + table -> tablespoon
+# meat + mince -> mincemeat
+# bird + oil -> oilbird
+# can + ape -> canape
+# pea + cow -> cowpea
+# cheese + board -> cheeseboard
+# fish + tile -> tilefish
+# fish + rock -> rockfish
+# dish + water -> dishwater
+# fish + blue -> bluefish
+# fish + white -> whitefish
+# bread + board -> breadboard
+# ling + green -> greenling
+# digest + ion -> digestion
+# copper + head -> copperhead
+# cheese + head -> headcheese
+# fish + pan -> panfish
+# fish + king -> kingfish
+# oat + cake -> oatcake
+# black + thorn -> blackthorn
+# cook + out -> cookout
+# bean + bag -> beanbag
+# pea + cock -> peacock
+# seed + moon -> moonseed
+# pen + elope -> penelope
+# sup + ping -> supping
+# fish + butter -> butterfish
+# meal + oat -> oatmeal
+# dessert + spoon -> dessertspoon
 
 # of these 18 partial solutions, "panfish" seems promising. 
 #    fish pan --> panfish is a legitimate solution.
