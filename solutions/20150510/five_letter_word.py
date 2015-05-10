e9da282a2d13e1f54e2970894da455e392309f31
johno_000 <johno_000@john-p34wv3cf2.home>
2015-05-10 15:12:45 -0500

    starting on a solver package that is reusable

---
+++ solutions/20150510/five_letter_word.py
@@ -0,0 +1,15 @@
+'''
+Created on May 10, 2015
+
+@author: johno_000
+'''
+
+from solver.solver import Solver
+
+
+
+
+
+if __name__ == '__main__':
+    s = Solver()
+    print("{0}".format(s.hash("cargo")))
---
+++ solver/solver.py
@@ -0,0 +1,19 @@
+'''
+Created on May 10, 2015
+
+@author: johno_000
+'''
+
+class Solver(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+
+    def hash(self, t):
+        return("".join(sorted(t)))
