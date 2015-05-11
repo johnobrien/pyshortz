a3bf8ca47b63b9729040e8eb7d8b6e1b9a307993
johno_000 <johno_000@john-p34wv3cf2.home>
2015-05-10 20:46:04 -0500

    Hmmm.... Started on a word class which subclasses string. Could be useful.

--- solutions/20150510/five_letter_word.py
+++ solutions/20150510/five_letter_word.py
@@ -4,7 +4,8 @@ Created on May 10, 2015
 @author: johno_000
 '''
 
-from solver.solver import Solver
+from nltk.corpus import words, wordnet
+from solver.solver import Solver, Word
 
 p = '''
 Think of a common one-syllable, 
@@ -18,15 +19,28 @@ but you only have to think of one.
 '''
 
 # Where to look for syllables: http://h6o6.com/2013/03/using-python-and-the-nltk-to-find-haikus-in-the-public-twitter-stream/
-
-
-
+# Also, http://www.onebloke.com/2011/06/counting-syllables-accurately-in-python-on-google-app-engine/
 
 class MySolver(Solver):
-    pass
+        
+    def Solve(self):
+        for word in words:
+            if word.len == 5:
+                w = Word(word)
+                syllable_flag = []
+                syllable_flag[1] = False
+                for anagram in w.anagrams:
+                    syllable_flag[anagram.syllables] = True
 
+                if syllable_flag[1] == True and\
+                   syllable_flag[2] == True and\
+                   syllable_flag[3] == True:
+                    self.candidates.append(word)
+                
+                
 
-
+                    
 if __name__ == '__main__':
     s = MySolver(p)
-    print("{0}".format(s.hash("cargo")))
+    for candidate in s.candidates:
+        print("{0}".format(candidate))
--- solver/solver.py
+++ solver/solver.py
@@ -4,6 +4,45 @@ Created on May 10, 2015
 @author: johno_000
 '''
 
+'''
+Let's build a list of all anagrams!
+'''
+import nltk
+from nltk.corpus import words, cmudict
+import shelve
+
+d = cmudict.dict()
+
+def nsyl(word):
+    return [len(list(y for y in x if y.isdigit(y[-1]))) for x in d[word.lower()]]
+
+def alphabetize(t):
+    return("".join(sorted(t)))
+
+
+s = shelve.open("words.db")
+if "anagrams" in s:
+    anagrams = s["anagrams"]
+else:
+    anagrams = {}
+    nltk.download('wordnet')
+    for word in words.words():
+        alpha = alphabetize(word)
+        if alpha in anagrams:
+            anagrams[alpha].update(word)
+        else:
+            anagrams[alpha] = set(word)
+    s["anagrams"] = anagrams
+s.close()
+
+class Word(str):
+    def __init__(self, t):
+        self.t = t
+        self.anagrams = anagrams[alphabetize[self.t]]
+        self.reversed = self.t[::-1]
+        self.alphabetized = alphabetize(self.t)
+        self.syllabes = nsyl(self.t)
+
 class Solver(object):
     '''
     classdocs
@@ -15,6 +54,7 @@ class Solver(object):
         Constructor
         '''
         self.p = puzzle_text
-
+        self.candidates = []
+    
     def hash(self, t):
         return("".join(sorted(t)))
