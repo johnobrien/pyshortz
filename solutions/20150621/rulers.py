25d16cbf1c57d7d574ed303fee9fe2bed8a0a966
john.obrien <john.obrien@MA10LP509047.na.wkglobal.com>
2015-06-22 15:03:51 -0400

    Why doesn't it work?

--- solutions/20150621/rulers.py
+++ solutions/20150621/rulers.py
@@ -4,10 +4,11 @@ Created on June 21, 2015
 @authors: leiran biton, john o'brien
 '''
 
-from solver.solver import Solver, char_filter
+from solver.solver import Solver
 from word import alphabetize
 import requests
 import re
+
 
 class RulerSolver(Solver):
     '''
@@ -15,8 +16,26 @@ class RulerSolver(Solver):
     '''
     pass
 
-def get_rulers_list():
-    pass
+    def solve(self):
+        target = alphabetize("Iamamonarch".lower())
+        # JO: Rulers.org looks good.
+        # Trying wikipedia for though because I can easily iterate
+        # using a numeric range.
+        # page = requests.get('http://www.rulers.org/')
+
+        for i in range(1, 2015):
+            print("Trying {0}".format(i))
+            page = requests.get('https://en.wikipedia.org/wiki/List_of_state_leaders_in_{0}'.format(i))
+            content = str(page.text.encode('ascii', 'ignore'))
+            results = re.findall(r'title="(.*?)"', content)
+            for result in results:
+                temp = alphabetize(result.replace(" ", "").lower())
+                if temp == target:
+                    print("Found: {0}->{1}".format(result, temp))
+                    solution = (result, temp)
+
+        print("Found solution: {0}->{1}".format(solution[0], solution[1]))
+
 
 if __name__ == '__main__':
     p = """
@@ -24,18 +43,6 @@ Take the phrase "I am a monarch." Re-arrange the
 11 letters to name a world leader who was 
 not a monarch but who ruled with similar authority. Who is it?
 """
-    target = alphabetize("Iamamonarch".lower())
-    # JO: Rulers.org looks good.
-    # Trying wikipedia for though because I can easily iterate
-    # using a numeric range.
-    # page = requests.get('http://www.rulers.org/')
 
-    for i in range(1900, 2015):
-        print("Trying {0}".format(i))
-        page = requests.get('https://en.wikipedia.org/wiki/List_of_state_leaders_in_{0}'.format(i))
-        content = str(page.text.encode('ascii', 'ignore'))
-        results = re.findall(r'title="(.*?)"', content)
-        for result in results:
-            temp = alphabetize(result.replace(" ", "").lower())
-            if temp == target:
-                print("Found: {0}->{1}".format(result, temp))
+    s = RulerSolver(puzzle_text = p)
+    s.solve()
