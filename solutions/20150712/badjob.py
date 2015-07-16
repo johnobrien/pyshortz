bee8a52edb8e38b16d956f4b7e12ae3b45852ec7
leiran <leiranbiton@gmail.com>
2015-07-16 09:34:24 -0400

    solution to 7-12

--- solutions/20150712/badjob.py
+++ solutions/20150712/badjob.py
@@ -6,11 +6,10 @@ Created on May 24, 2015
 
 import csv
 from nltk.corpus import words
-from solver.solver import Solver, char_filter
-from solver.word import alphabetize, build_anagrams
+from solver.solver import Solver, char_filter, get_all_words
 from string import punctuation
 
-class MySolver(Solver):
+class JobSolver(Solver):
     '''
     MySolver is an example solver
     which is a child of the solver class.
@@ -20,46 +19,35 @@ class MySolver(Solver):
         self.words = wordlist
     
     def loadjobs(self, joblist):
-        self.jobs = joblist
+        jobs_oneword = {}
+        for job in joblist:
+            if job.startswith("b"):
+                job_stripped = char_filter(job, " "+punctuation)
+                if job_stripped in jobs_oneword:
+                    jobs_oneword[job_stripped].add(job)
+                else:
+                    jobs_oneword[job_stripped] = set([job])
+            self.jobs = jobs_oneword
     
     def process(self, job):
-        job_stripped = job.remove(" "+punctuation)
+        job_stripped = char_filter(job, " "+punctuation)
         if len(job_stripped) < 4:
             return ""
         else:
             return job_stripped[0] + job_stripped[4:]
-        
     
-    def solver(self):
-        for job in self.jobs:
-            
+    def solve(self, threshold=1):
+        for job in self.jobs.keys():
+            processed = self.process(job)
+            if processed in self.words and len(processed >= threshold):
+                for val in self.jobs[job]:
+                    self.candidates.add((val, processed))
     
-    def try_list(self, list_name, jobs):
-        '''
-        Takes a list job titles,
-        and trys to find whether any of them together
-        match merchant raider alphabetized.
-        '''
-        if self.__dict__.get("verbose", False): 
-            print("generating anagrams")
-        self.anagrams = build_anagrams(jobs)
-        for job in jobs:
-            try:
-                remaining_letters = alphabetize(char_filter(self.word, job, 1))
-                if remaining_letters in self.anagrams:
-                    for second_job in self.anagrams[remaining_letters]:
-                        if ((job, second_job) not in self.candidates and (second_job, job) not in self.candidates):
-                            self.candidates.add((job, second_job))
-            except AssertionError:
-                pass
-        if self.__dict__.get("verbose", False): self.get_candidates()
-
     def get_candidates(self):
         """prints the current list of candidates"""
         print("current candidates:")
-        for job1, job2 in self.candidates:
-            print("    {0} {1}".format(job1, job2))
-
+        for job, word in self.candidates:
+            print("    {0} --> {1}".format(job, word))
 
 if __name__ == '__main__':
 
@@ -69,12 +57,7 @@ remaining letters in order will name something you might
 experience in the presence of someone who has this
 occupation. What is it?'''
 
-    # quiet testing
-    s = MySolver(p, word="blosserfakers", verbose=False)
-    s.try_list("testing", ["baker", "flosser", "flossers", "bakers"])
-
-    #Try NLTK
-    s = MySolver(p, word="merchantraider", verbose=False)
+    s = JobSolver(p)
     print("accessing nltk with keyword list:")
     kws = ["occupation",
            "job",
@@ -95,10 +78,9 @@ occupation. What is it?'''
            ]
     for kw in kws:
         print("    {0}".format(kw))
-    unfiltered_wordnet_jobs = s.get_words(kws)
-    wordnet_jobs = [char_filter(job, punctuation+" ") for job in unfiltered_wordnet_jobs]
-    job_dict = dict(zip(wordnet_jobs, unfiltered_wordnet_jobs))
-    print("retrieved {0} potential entries".format(len(wordnet_jobs)))
-    s.try_list("wordnet", wordnet_jobs)
-    for job1, job2 in s.candidates:
-        print(job_dict[job1], job_dict[job2])
+    wordnet_jobs = s.get_words(kws)
+    s.loadjobs(wordnet_jobs)
+    s.loadwords(get_all_words())
+    s.solve(threshold=3)
+    s.get_candidates()
+    
