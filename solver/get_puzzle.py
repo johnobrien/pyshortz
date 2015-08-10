126ca4998d890bcd94e42ee5dc692291ac3cfb44
John.OBrien <John.OBrien@MA10LP509047.na.wkglobal.com>
2015-08-10 19:41:02 -0400

    Wrote out some thoughts on different approaches.

--- solver/get_puzzle.py
+++ solver/get_puzzle.py
@@ -12,6 +12,8 @@ from nltk.tokenize import word_tokenize
 
 NPR_key = "MDE5ODcyNTE2MDE0MzY4MzY2NTY1MDVjMQ001"
 
+# Good source of lists: https://github.com/dariusk/corpora
+
 
 def get_puzzle(date):
     '''
@@ -20,7 +22,19 @@ def get_puzzle(date):
 
     date is string in the format 2015-07-12
     date has to be a Sunday
+
+    One idea from Michelle from Python meetup, use named entity extraction
+    (http://www.nltk.org/book/ch07.html#fig-locations) to get the source
+    and target lists (provide some training data for that).
+    http://mattshomepage.com/#/blog/feb2013/liftingthehood
+
+    Another pass to get criteria to restrict the source and target lists.
+    And then parse out the middle part get the transform part of the puzzle.
+
+    Spacy is apparently good to depency parsing.
+
     '''
+
     response = requests.get("http://api.npr.org/query?id=4473090&date={0}&dateType=story&output=JSON&apiKey=MDE5ODcyNTE2MDE0MzY4MzY2NTY1MDVjMQ001".format(date))
     d = json.loads(response.text)
     p = d["list"]["story"][0]["text"]["paragraph"]
@@ -34,8 +48,9 @@ def get_puzzle(date):
             for i, s in enumerate(sentences):
                 print("Sentence {0}: {1}".format(i, s))
                 s = word_tokenize(s)
-                print(nltk.pos_tag(s))
+                # print(nltk.pos_tag(s))
                 # http://www.monlp.com/2011/11/08/part-of-speech-tags/
+            print("\n")
 
 if __name__ == "__main__":
     date = datetime.date(2015, 6, 28)
